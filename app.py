6d4fd3399bb30662cd374c670d8f631bfc92b116
Arun-Kumar-B <arunkumar.boralingappa@gmail.com>
2022-06-22 17:43:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,11 @@ def success(name):
 def projects():
     return render_template("login.html", title = 'login')
  
+@app.route('/')
+def home():
+   return render_template('home.html')
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run()
+  
+  
