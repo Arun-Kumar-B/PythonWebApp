7d8f3da8b1b397910a3a2a38f44b95d10d71d05c
Arun-Kumar-B <arunkumar.boralingappa@gmail.com>
2022-06-22 17:29:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ def success(name):
     return 'welcome %s' % name
  
  
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
- 
+@app.route('/login')
+def projects():
+    return render_template("login.html", title = 'login')
  
 if __name__ == '__main__':
     app.run(debug=True)
